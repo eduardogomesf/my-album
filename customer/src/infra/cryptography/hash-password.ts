6f3773f19af760c5f23f75c249cf922b7fe2f0b4
Eduardo Gomes <edugomesf@icloud.com>
2023-11-15 17:11:06 -0300

    fix: change import of bcrypt's hash function

--- customer/src/infra/cryptography/hash-password.ts
+++ customer/src/infra/cryptography/hash-password.ts
@@ -1,9 +1,9 @@
-import bcrypt from 'bcrypt'
+import { hash } from 'bcrypt'
 
 import { type HashPassword } from '../../application/protocol'
 
 export class BcryptHashPassword implements HashPassword {
   async hash(password: string): Promise<string> {
-    return await bcrypt.hash(password, 10)
+    return await hash(password, 10)
   }
 }
