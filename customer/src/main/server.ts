50060bd05a9d8c808844e02e4078f1ec17cc2cda
Eduardo Gomes <edugomesf@icloud.com>
2023-11-15 17:11:36 -0300

    feat: establish mongodb connection before starting up server during bootstrap

--- customer/src/main/server.ts
+++ customer/src/main/server.ts
@@ -1,8 +1,11 @@
+import { connectToMongo } from '../infra/database/mongodb/client'
 import { type UseCases } from '../presentation/interface/use-cases'
 import { startExpressServer } from '../presentation/rest/setup'
 import { generateCreateNewCustomerUseCase } from './factory/use-case/create-new-customer-use-case.factory'
 
-export function bootstrap() {
+export async function bootstrap() {
+  await connectToMongo()
+
   const useCases: UseCases = {
     createNewCustomer: generateCreateNewCustomerUseCase()
   }
@@ -10,4 +13,8 @@ export function bootstrap() {
   startExpressServer(useCases)
 }
 
-bootstrap()
+bootstrap().catch(error => {
+  console.error('Error bootstrapping application')
+  console.error(error)
+  process.exit(1)
+})
