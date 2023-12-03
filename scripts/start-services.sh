a61a33fb4ef329f911ca2cca18dfa8f70ed6cc74
Eduardo Gomes <edugomesf@icloud.com>
2023-12-03 00:04:32 -0300

    feat: add start services script

---
+++ scripts/start-services.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Start all services with Docker Compose
+docker-compose up -d
+
+# Wait for Kong to be fully operational
+echo "Waiting for Kong to start..."
+sleep 15  # Adjust the sleep duration as necessary
+
+# Now run the Kong configuration script
+echo "Configuring Kong..."
+bash ./configure-kong.sh
+
+echo "Services started and configured."
