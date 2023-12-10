6d2b5260cb422e861f0948f75376d8f9cf715987
Eduardo Gomes <edugomesf@icloud.com>
2023-12-10 11:27:55 -0300

    feat: add Logger

---
+++ customer/src/shared/logger.ts
@@ -0,0 +1,23 @@
+import chalk from 'chalk'
+
+export class Logger {
+  static verbose(message: string) {
+    console.debug(chalk.cyan(message))
+  }
+
+  static warn(message: string) {
+    console.warn(chalk.yellow(message))
+  }
+
+  static info(message: string) {
+    console.info(chalk.green(message))
+  }
+
+  static error(message: any) {
+    console.error(chalk.red(message))
+  }
+
+  static fatal(message: any) {
+    console.error(chalk.bgRed(message))
+  }
+}
