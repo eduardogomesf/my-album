bbf3e532721b94a3435867a50d38f4de88b95038
Eduardo Gomes <edugomesf@icloud.com>
2023-12-16 21:04:02 -0300

    feat: add base notification service files

---
+++ notification/src/infra/messaging/kafka/client.ts
@@ -0,0 +1,28 @@
+import { Kafka } from 'kafkajs'
+import { ENVS, Logger } from '../../../shared'
+
+let kafkaClient: Kafka = null as any
+
+export async function generateKafkaClient(): Promise<Kafka> {
+  if (kafkaClient) {
+    Logger.info('Kafka client already exists')
+
+    return kafkaClient
+  }
+
+  Logger.info('Creating Kafka client...')
+
+  kafkaClient = new Kafka({
+    clientId: ENVS.APP.ID,
+    brokers: [...ENVS.KAFKA.BROKERS_URL],
+    connectionTimeout: 5000,
+    retry: {
+      initialRetryTime: 300,
+      retries: 8
+    }
+  })
+
+  Logger.info('Kafka client created!')
+
+  return kafkaClient
+}
---
+++ notification/src/shared/index.ts
@@ -0,0 +1,3 @@
+export * from './env'
+export * from './delay'
+export * from './logger'
