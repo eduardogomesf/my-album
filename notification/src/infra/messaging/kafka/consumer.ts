f7608553212b0b80bb56f607550c1ce867b457a5
Eduardo Gomes <edugomesf@icloud.com>
2023-12-18 20:52:43 -0300

    feat: add more options to createNewKafkaConsumer function

--- notification/src/infra/messaging/kafka/consumer.ts
+++ notification/src/infra/messaging/kafka/consumer.ts
@@ -1,7 +1,19 @@
 import { type Consumer, type Kafka } from 'kafkajs'
 
-export async function createNewKafkaConsumer(client: Kafka, consumerGroup: string): Promise<Consumer> {
-  const consumer = client.consumer({ groupId: consumerGroup })
+export interface ConsumerOptions {
+  consumerGroup: string
+  subscribeOptions: {
+    topic: string
+    fromBeginning: boolean
+  }
+}
+
+export async function createNewKafkaConsumer(client: Kafka, consumerOptions: ConsumerOptions): Promise<Consumer> {
+  const consumer = client.consumer({ groupId: consumerOptions.consumerGroup })
   await consumer.connect()
+  await consumer.subscribe({
+    topic: consumerOptions.subscribeOptions.topic,
+    fromBeginning: consumerOptions.subscribeOptions.fromBeginning
+  })
   return consumer
 }
