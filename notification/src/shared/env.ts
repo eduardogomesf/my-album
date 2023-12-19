1d549d512a81f58436ced908533a4d9e02e21050
Eduardo Gomes <edugomesf@icloud.com>
2023-12-18 21:07:23 -0300

    refactor: remove unused envs from config file

--- notification/src/shared/env.ts
+++ notification/src/shared/env.ts
@@ -7,14 +7,6 @@ export const ENVS = {
     PORT: process.env.PORT ?? 3000,
     ID: process.env.APP_ID ?? 'customer-service'
   },
-  MONGO: {
-    CONNECTION_NAME: process.env.MONGO_CONNECTION_NAME ?? 'customer-service-mongo',
-    URL: process.env.MONGO_URL ?? 'mongodb://localhost:27017',
-    DB_NAME: process.env.MONGO_DB_NAME ?? 'customer-service-mongo'
-  },
-  ACCESS_TOKEN: {
-    SECRET_KEY: process.env.ACCESS_TOKEN_SECRET_KEY ?? ''
-  },
   KAFKA: {
     BROKERS_URL: process.env.KAFKA_HOSTS ? String(process.env.KAFKA_HOSTS).split(',') : [],
     TOPICS: {
