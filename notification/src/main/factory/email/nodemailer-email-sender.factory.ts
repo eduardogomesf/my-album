3cee059572c9d9956b5d666e1ad7237754f1481f
Eduardo Gomes <edugomesf@icloud.com>
2023-12-22 20:54:09 -0300

    fix: update nodemailer import

--- notification/src/main/factory/email/nodemailer-email-sender.factory.ts
+++ notification/src/main/factory/email/nodemailer-email-sender.factory.ts
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer'
+import * as nodemailer from 'nodemailer'
 import { NodemailerEmailSender } from '@/infra/email/nodemailer'
 import { type EmailSender } from '@/application/protocol'
 
