7a29754cd75488e68d9e51f8eaca813dd9c7e604
Eduardo Gomes <edugomesf@icloud.com>
2023-12-28 04:17:35 -0300

    FEAT: start file manager microservice with base files (#13)
    * setup: add base files for file manager microservice
    
    * [file-manager] feat: add application structure with base src files with express

---
+++ file-manager/src/main/config/express.ts
@@ -0,0 +1,36 @@
+import * as express from 'express'
+import * as cors from 'cors'
+
+import { ENVS, Logger } from '@/shared'
+import { type UseCases } from '@/presentation/interface/use-cases'
+
+function setDefaultMiddlewares(app: express.Express) {
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(cors())
+}
+
+function setDefaultRoutes(app: express.Express) {
+  app.get('/', (req, res) => {
+    res.send('Up and running!')
+  })
+}
+
+function setRoutes(app: express.Express, useCases: UseCases) {
+}
+
+export function bootstrapExpressServer(useCases: UseCases) {
+  const logger = new Logger('bootstrapExpressServer')
+
+  const app = express()
+
+  setDefaultMiddlewares(app)
+  setDefaultRoutes(app)
+  setRoutes(app, useCases)
+
+  const port = ENVS.APP.PORT
+
+  app.listen(port, () => {
+    logger.info(`App listening at port ${port}`)
+  })
+}
