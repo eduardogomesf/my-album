1548586cec7c1de83deee8e7aa27376be82fb752
eduardogomesf <eduardogoomesf@gmail.com>
2023-12-30 15:27:32 -0300

    feat: remove kongs services from shared services script

--- @scripts/start-shared-services.sh
+++ @scripts/start-shared-services.sh
@@ -3,7 +3,7 @@
 # Start all shared services with Docker Compose
 echo "Starting shared services..."
 
-docker-compose up zookeeper kafka kong-database kong-migration kong konga -d
+docker-compose up zookeeper kafka -d
 
 echo "Shared services started"
 
