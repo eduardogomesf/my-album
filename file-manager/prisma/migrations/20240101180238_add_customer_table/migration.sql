2cf591d9b7664bf821ec33711be3b48f8f46449c
eduardogomesf <eduardogoomesf@gmail.com>
2024-01-01 15:04:20 -0300

    [file-manager] feat: add prisma configuration and Customer model

---
+++ file-manager/prisma/migrations/20240101180238_add_customer_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_email_key" ON "customers"("email");
