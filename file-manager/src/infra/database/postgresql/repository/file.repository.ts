ce472746b7c230efe320da7001e49e72eb15d488
Eduardo Gomes <edugomesf@icloud.com>
2024-01-18 21:15:04 -0300

    feat: implements GetCurrentStorageUsageRepository with prisma

--- file-manager/src/infra/database/postgresql/repository/file.repository.ts
+++ file-manager/src/infra/database/postgresql/repository/file.repository.ts
@@ -1,11 +1,11 @@
-import { type SaveFileRepository } from '@/application/protocol/files'
+import { type GetCurrentStorageUsageRepository, type GetCurrentStorageUsageRepositoryResponse, type SaveFileRepository } from '@/application/protocol/files'
 import { type File } from '@/domain/entity'
 import { Logger } from '@/shared'
 import { prisma } from '../client'
 
 export const logger = new Logger('PrismaFileRepository')
 
-export class PrismaFileRepository implements SaveFileRepository {
+export class PrismaFileRepository implements SaveFileRepository, GetCurrentStorageUsageRepository {
   async save (file: File): Promise<void> {
     try {
       await prisma.file.create({
@@ -21,6 +21,26 @@ export class PrismaFileRepository implements SaveFileRepository {
           userId: file.userId
         }
       })
+    } catch (error) {
+      logger.error(error.message)
+      throw new Error(error)
+    }
+  }
+
+  async getUsage (userId: string): Promise<GetCurrentStorageUsageRepositoryResponse> {
+    try {
+      const result = await prisma.file.groupBy({
+        by: 'userId',
+        where: {
+          userId
+        },
+        _sum: {
+          size: true
+        }
+      })
+      return {
+        usage: Number(result[0]._sum.size)
+      }
     } catch (error) {
       logger.error(error.message)
       throw new Error(error)
