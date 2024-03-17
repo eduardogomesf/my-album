2496dd0f2d6f0e115fe76aa72a0a81112662e595
Eduardo Gomes <edugomesf@icloud.com>
2024-03-17 15:48:56 -0300

    [file-manager] feat: add getManyById implementation to FileRepository

--- file-manager/src/infra/database/postgresql/repository/file.repository.ts
+++ file-manager/src/infra/database/postgresql/repository/file.repository.ts
@@ -1,11 +1,31 @@
-import { type GetCurrentStorageUsageRepository, type GetCurrentStorageUsageRepositoryResponse, type SaveFileRepository } from '@/application/protocol/files'
-import { type File } from '@/domain/entity'
+import { type GetFilesByAlbumIdRepository, type GetCurrentStorageUsageRepository, type GetCurrentStorageUsageRepositoryResponse, type SaveFileRepository } from '@/application/protocol/files'
+import { File } from '@/domain/entity'
 import { Logger } from '@/shared'
 import { prisma } from '../client'
 
 const logger = new Logger('PrismaFileRepository')
 
-export class PrismaFileRepository implements SaveFileRepository, GetCurrentStorageUsageRepository {
+export class PrismaFileRepository implements SaveFileRepository, GetCurrentStorageUsageRepository, GetFilesByAlbumIdRepository {
+  async getManyById (albumId: string): Promise<File[]> {
+    try {
+      const files = await prisma.file.findMany({
+        where: {
+          albumId,
+          isDeleted: false
+        }
+      })
+      return files
+        ? files.map(file => new File({
+          ...file,
+          size: Number(file.size)
+        }))
+        : []
+    } catch (error) {
+      logger.error(error.message)
+      throw new Error(error)
+    }
+  }
+
   async save (file: File): Promise<void> {
     try {
       await prisma.file.create({
