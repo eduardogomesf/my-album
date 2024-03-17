b984fa93df619f5c7d1b95cd293fdec1fa5f621f
Eduardo Gomes <edugomesf@icloud.com>
2024-03-17 00:51:52 -0300

    [file-manager] feat: formatting albums before returning data

--- file-manager/src/application/use-case/get-albums.use-case.ts
+++ file-manager/src/application/use-case/get-albums.use-case.ts
@@ -9,9 +9,15 @@ export class GetAlbumsUseCase {
   async execute(userId: string): Promise<UseCaseResponse> {
     const albums = await this.getAlbumsRepository.getAll(userId)
 
+    const formattedAlbums = albums.map(album => ({
+      id: album.id,
+      name: album.name,
+      isMain: album.isMain
+    }))
+
     return {
       ok: true,
-      data: albums
+      data: formattedAlbums
     }
   }
 }
