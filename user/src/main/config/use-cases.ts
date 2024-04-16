0c3b99ee77b344b4a52bfed0f80ad89ce5e2b258
eduardogomesf <eduardogoomesf@gmail.com>
2024-04-16 13:29:04 -0300

    [user-service] refactor: improve dependency injections

--- user/src/main/config/use-cases.ts
+++ user/src/main/config/use-cases.ts
@@ -1,9 +1,39 @@
 import { type UseCases } from '@/presentation/interface/use-cases'
+import { ENVS } from '@/shared'
 import { generateCreateNewUserUseCase, generateUserLoginUseCase } from '../factory/use-case'
+import { generateKafkaProducer } from '../factory/messaging'
+import { generateMongoUserRepository } from '../factory/repository'
+import { generateBcrypPasswordValidator, generateBcryptHashPassword, generateJwtTokenGenerator, generateSendEmailNotificationUtil } from '../factory/util'
 
 export const getApplicationUseCases = async (): Promise<UseCases> => {
+  // Repositories
+  const userRepository = generateMongoUserRepository()
+
+  // Utils
+  const hashPassword = generateBcryptHashPassword()
+  const passwordValidator = generateBcrypPasswordValidator()
+  const jwtTokenGenerator = generateJwtTokenGenerator()
+
+  // Message senders
+  const newUserCreatedSender = await generateKafkaProducer(ENVS.KAFKA.TOPICS.USER.CREATED)
+  const sendWelcomeNotification = await generateSendEmailNotificationUtil()
+
+  // Use cases
+  const createNewUser = generateCreateNewUserUseCase(
+    userRepository,
+    hashPassword,
+    userRepository,
+    newUserCreatedSender,
+    sendWelcomeNotification
+  )
+  const userLogin = generateUserLoginUseCase(
+    userRepository,
+    passwordValidator,
+    jwtTokenGenerator
+  )
+
   return {
-    createNewUser: await generateCreateNewUserUseCase(),
-    userLogin: generateUserLoginUseCase()
+    createNewUser,
+    userLogin
   }
 }
