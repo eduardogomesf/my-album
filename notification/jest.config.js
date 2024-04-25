a5a28156611052b25ebe1971aa1145dd137f0944
Eduardo Gomes <edugomesf@icloud.com>
2024-04-25 13:54:07 -0300

    chore: add test:cov script and update test coverage files

--- notification/jest.config.js
+++ notification/jest.config.js
@@ -5,5 +5,15 @@ module.exports = {
   testMatch: ['<rootDir>/tests/**/*.spec.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
-  }
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/',
+    '<rootDir>/src/main',
+    '<rootDir>/src/infra',
+    '<rootDir>/src/shared',
+    '<rootDir>/src/presentation',
+    '<rootDir>/src/application/interface',
+    '<rootDir>/src/application/protocol'
+  ]
 }
