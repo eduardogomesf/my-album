5759cee05f5ce926f38d0cccc3fca934afc95e1b
Eduardo Gomes <edugomesf@icloud.com>
2024-05-07 12:40:52 -0300

    feat: remove scripts

--- @scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Wait for Kong to be fully operational
-echo "Setting up services with configurations..."
-bash ./@scripts/configure-kong.sh
-
-echo "Setup completed!"
--- @scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Setup
-bash ./@scripts/setup.sh
-
-echo "Starting services..."
-docker compose up -d --build > /dev/null 2>&1
-
-echo "Services up and running!"
