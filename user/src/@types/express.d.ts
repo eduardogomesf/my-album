a583bed23ff107118f3a44301836acbbf0724985
Eduardo Gomes <edugomesf@icloud.com>
2024-05-14 23:51:26 -0300

    [user-service] feat: add Auth middleware

--- user/src/@types/express.d.ts
+++ user/src/@types/express.d.ts
@@ -3,6 +3,9 @@ import { Request } from 'express-serve-static-core'
 
 declare module 'express-serve-static-core' {
   export interface Request {
+    auth: {
+      userId: string
+    }
     tracking: {
       correlationId: string
     }
---
+++ user/src/presentation/rest/middleware/get-auth-info-from-headers.ts
@@ -0,0 +1,37 @@
+import { Logger } from '@/shared'
+import { type NextFunction, type Request, type Response } from 'express'
+import { generateJwtTokenValidator } from '@/main/factory/util'
+
+export interface AuthInfo {
+  userId: string
+  email: string
+  role: string
+}
+
+const logger = new Logger('getAuthInfoFromToken')
+
+const tokenValidator = generateJwtTokenValidator()
+
+export async function asyncgetAuthInfoFromHeaders(req: Request, res: Response, next: NextFunction): Promise<any> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, token] = req.headers.authorization?.split(' ') as string[]
+
+  const { isValid, data, invalidationReason } = await tokenValidator.validate(token)
+
+  if (!isValid) {
+    logger.error('Auth token invalid or expired')
+    const message = invalidationReason === 'EXPIRED' ? 'Token expired' : 'Token invalid'
+    return res.status(403).send(message)
+  }
+
+  const { id: userId } = data
+
+  if (!userId) {
+    logger.error('User id not found in token')
+    return res.status(403).send('User id not found in token')
+  }
+
+  req.auth = { userId }
+
+  next()
+}
