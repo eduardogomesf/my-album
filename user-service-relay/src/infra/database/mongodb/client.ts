120ea839d5c25245d1315da11d08fa9d6a6807b5
Eduardo Gomes <edugomesf@icloud.com>
2024-05-19 20:41:41 -0300

    [user-service-relay] feat: add UnpublishedMessage entity and repository

---
+++ user-service-relay/src/infra/database/mongodb/client.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+import { ENVS, Logger } from '@/shared'
+
+const logger = new Logger('MongoConnectionManager')
+export class MongoConnectionManager {
+  private static readonly connections: Record<string, mongoose.Connection> = {}
+
+  static getOrCreate(
+    connectionName: string,
+    connectionURL?: string,
+    dbName?: string,
+    connectionOptions?: mongoose.ConnectOptions
+  ) {
+    try {
+      if (MongoConnectionManager.connections[connectionName]) {
+        logger.info(`MongoDB connection ${connectionName} already exists`)
+        return MongoConnectionManager.connections[connectionName]
+      }
+
+      logger.info('Connecting to MongoDB...')
+
+      if (!connectionURL || !dbName || !connectionOptions) {
+        throw new Error('Missing connectionURL, dbName or connectionOptions')
+      }
+
+      const createdConnection = mongoose.createConnection(connectionURL, {
+        ...connectionOptions,
+        dbName,
+        appName: ENVS.APP.ID
+      })
+
+      MongoConnectionManager.connections[connectionName] = createdConnection
+
+      logger.info('Connected to MongoDB!')
+      return createdConnection
+    } catch (error) {
+      logger.error('Error connecting to MongoDB')
+      logger.error(error)
+      throw error
+    }
+  }
+}
---
+++ user-service-relay/src/infra/database/mongodb/unpublished-messages/unpublished-messages.repository.ts
@@ -0,0 +1,48 @@
+import { Logger } from '@/shared'
+import { UnpublishedMessageModel } from './unpublished-messages.entity'
+import {
+  type DeleteUnpublishedMessageRepository,
+  type UpdateUnpublishedMessageRepository,
+  type GetPendingUnpublishedMessagesRepository
+} from '@/use-case/protocol'
+import { type UnpublishedMessage } from '@/entity'
+
+const logger = new Logger('MongoUnpublishedMessagesRepository')
+
+export class MongoUnpublishedMessagesRepository implements
+  GetPendingUnpublishedMessagesRepository, DeleteUnpublishedMessageRepository, UpdateUnpublishedMessageRepository {
+  async findAllPending(): Promise<UnpublishedMessage[]> {
+    try {
+      return await UnpublishedMessageModel.find({
+        hasError: false
+      })
+    } catch (error) {
+      logger.error('Error finding unpublished messages')
+      logger.error(error)
+      throw error
+    }
+  }
+
+  async update(message: UnpublishedMessage): Promise<void> {
+    try {
+      await UnpublishedMessageModel.updateOne(
+        { _id: message.id },
+        message
+      )
+    } catch (error) {
+      logger.error('Error finding updating messages')
+      logger.error(error)
+      throw error
+    }
+  }
+
+  async delete (id: string): Promise<void> {
+    try {
+      await UnpublishedMessageModel.deleteOne({ _id: id })
+    } catch (error) {
+      logger.error('Error finding deleting messages')
+      logger.error(error)
+      throw error
+    }
+  }
+}
