e63a86a08af3864f03a6c3f11b5b4a202c9797d0
Eduardo Gomes <edugomesf@icloud.com>
2024-05-19 16:01:23 -0300

    [user-service-relay] feat: add error prop to UnpublishedMessage

--- user-service-relay/src/entity/unpublished-message.entity.ts
+++ user-service-relay/src/entity/unpublished-message.entity.ts
@@ -1,9 +1,10 @@
-import { BaseEntity } from "./base.entity"
+import { BaseEntity } from './base.entity'
 
-type CreateUnpublishedMessageDto = {
-  id: string,
-  data: Record<string, any>,
-  hasError?: boolean,
+interface CreateUnpublishedMessageDto {
+  id: string
+  data: Record<string, any>
+  hasError?: boolean
+  error?: string
   options: Record<string, any> & { topic: string }
   createdAt: Date
   updatedAt: Date
@@ -12,7 +13,8 @@ type CreateUnpublishedMessageDto = {
 export class UnpublishedMessage extends BaseEntity {
   id: string
   data: Record<string, any>
-  hasError: boolean
+  hasError?: boolean
+  error?: string
   options: Record<string, any>
   createdAt: Date
   updatedAt: Date
@@ -20,7 +22,8 @@ export class UnpublishedMessage extends BaseEntity {
   constructor(dto: CreateUnpublishedMessageDto) {
     super(dto.id)
     this.data = dto.data
-    this.hasError = dto.hasError || false
+    this.hasError = dto.hasError ?? false
+    this.error = dto.error ?? ''
     this.options = dto.options
     this.createdAt = dto.createdAt
     this.updatedAt = dto.updatedAt
