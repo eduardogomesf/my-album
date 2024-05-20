52997dbb9a90dd12183fcdff2197a0b449ccf960
Eduardo Gomes <edugomesf@icloud.com>
2024-05-19 21:20:39 -0300

    [user-service-relay] update Kafka MessageSender to receive topic in parameters

--- user-service-relay/src/infra/messaging/kafka/sender.ts
+++ user-service-relay/src/infra/messaging/kafka/sender.ts
@@ -6,14 +6,19 @@ import { Logger } from '@/shared'
 
 const logger = new Logger('KafkaProducer')
 
+interface SendOptions {
+  topic: string
+}
 export class KafkaProducer implements MessageSender {
   constructor(
-    private readonly kafkaResources: KafkaResources,
-    private readonly topic: string
+    private readonly kafkaResources: KafkaResources
   ) {
   }
 
-  async send(rawMessage: Record<string, any>): Promise<boolean> {
+  async send(
+    rawMessage: Record<string, any>,
+    options: SendOptions
+  ): Promise<boolean> {
     const { producer } = this.kafkaResources
 
     const timestamp = Date.now()
@@ -27,10 +32,10 @@ export class KafkaProducer implements MessageSender {
     try {
       const message = JSON.stringify(messageWithId)
 
-      logger.info(`Publishing message with id ${messageWithId.id} to topic ${this.topic} ...`)
+      logger.info(`Publishing message with id ${messageWithId.id} to topic ${options.topic} ...`)
 
       await producer.send({
-        topic: this.topic,
+        topic: options.topic,
         messages: [
           {
             value: message,
@@ -39,10 +44,10 @@ export class KafkaProducer implements MessageSender {
         ]
       })
 
-      logger.info(`Message of id ${messageWithId.id} published to Kafka topic ${this.topic}`)
+      logger.info(`Message of id ${messageWithId.id} published to Kafka topic ${options.topic}`)
       return true
     } catch (error) {
-      logger.warn(`Error while sending message to Kafka topic ${this.topic}}`)
+      logger.warn(`Error while sending message to Kafka topic ${options.topic}}`)
       logger.warn(error.message)
       return false
     }
