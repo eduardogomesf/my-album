07efd2afe158bddd6b3960d3f11976950fa93e6c
Eduardo Gomes <edugomesf@icloud.com>
2024-05-31 15:06:55 -0300

    [web] feat: add redirect to sign-in page and improve responsive styles

--- web/src/app/(pages)/sign-up/page.tsx
+++ web/src/app/(pages)/sign-up/page.tsx
@@ -2,12 +2,13 @@
 
 import { TextInput } from "@/app/components/form/text-input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from 'zod'
 
 const signUpSchema = z.object({
-  firstName: z.string({ message: 'First name is required' }),
-  lastName: z.string({ message: 'Last name is required' }),
+  firstName: z.string().min(2, { message: 'First name is required' }),
+  lastName: z.string().min(2, { message: 'Last name is required' }),
   email: z.string().email({ message: 'Invalid email' }),
   phoneNumber: z.string().min(10, { message: 'Invalid phone number' }),
   password: z.string().min(8, { message: 'Password must have at least 8 characters' }),
@@ -30,13 +31,17 @@ export default function SignUp() {
   }
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center">
       <main className="flex flex-col items-center max-w-[500px] w-full p-6">
-        <h1 className="text-2xl text-gray-950 md:text-3xl	font-bold">Sign up</h1>
-        <p className="mt-3 text-gray-500 font-normal text-md md:text-lg">Create your account for free</p>
+        <h1 className="text-2xl text-gray-950 md:text-3xl	font-bold">
+          Sign up
+        </h1>
+        <p className="mt-3 text-gray-500 font-normal text-md md:text-lg">
+          Create your account for free
+        </p>
 
         <form
-          className="mt-8 w-full flex flex-col gap-2"
+          className="mt-4 md:mt-8 w-full flex flex-col gap-2 md:gap-3"
           onSubmit={handleSubmit(handleSignUp)}
           noValidate
         >
@@ -88,13 +93,20 @@ export default function SignUp() {
           />
 
           <button
-            className="mt-4 bg-gray-950 rounded-md text-white p-3 hover:bg-gray-800"
+            className="md:mt-4 bg-gray-950 rounded-md text-white p-3 hover:bg-gray-800"
             type="submit"
             disabled={isSubmitting}
           >
             Sign up
           </button>
         </form>
+
+        <span className="text-md font-normal mt-6">
+          Already have an account?{' '}
+          <Link href="/sign-in" className="text-gray-600 hover:underline hover:text-gray-800">
+            Sign in
+          </Link>
+        </span>
 
       </main>
     </div>
--- web/src/app/components/form/text-input.tsx
+++ web/src/app/components/form/text-input.tsx
@@ -13,7 +13,7 @@ type InputProps = InputCustomProps & InputHTMLAttributes<HTMLInputElement>
 const TextInput = forwardRef<HTMLInputElement, InputProps>(
   ({ label, labelId, error, ...props }, ref) => {
     return (
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col items-start gap-1">
         {label && <Label htmlFor={labelId}>{label}</Label>}
         <input
           className="w-full rounded-md p-3 focus:outline-none focus:ring-2 focus:ring-gray-950 bg-white"
