f15999a845ad132816e4394dc58242b05970da4e
Eduardo Gomes <edugomesf@icloud.com>
2024-06-01 18:38:59 -0300

    [web] feat: add Toaster provider to root layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "./fonts";
 import QueryProvider from "./providers/ReactQueryProvider";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "My Album",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased min-h-screen`}>
         <QueryProvider>
+          <Toaster richColors />
           {children}
         </QueryProvider>
       </body>
