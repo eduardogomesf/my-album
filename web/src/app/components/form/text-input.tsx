705dc00a578a060618ae58f955c52827f770bc02
eduardogomesf <eduardogoomesf@gmail.com>
2024-06-07 18:52:05 -0300

    [web] feat: improve TextInput font and border styles

--- web/src/app/components/form/text-input.tsx
+++ web/src/app/components/form/text-input.tsx
@@ -17,14 +17,14 @@ const TextInput = forwardRef<HTMLInputElement, InputProps>(
       <div className="flex flex-col items-start gap-1">
         {label && <Label htmlFor={labelId}>{label}</Label>}
         <input
-          className="w-full rounded-md bg-white p-3 focus:outline-none focus:ring-2 focus:ring-gray-950"
+          className="w-full rounded-md bg-white p-3 text-sm focus:outline-none border focus:border-gray-800"
           id={labelId}
           ref={ref}
           {...props}
         />
         <span
           className={clsx(
-            'mt-1 text-xs text-red-500 opacity-0 md:text-sm',
+            'mt-1 ml-2 text-xs text-red-500 opacity-0',
             error && 'opacity-100',
           )}
         >
