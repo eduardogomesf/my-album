bf217c7673546ed2522d63e0eb625bed4f7ee94b
Eduardo Gomes <edugomesf@icloud.com>
2024-06-07 22:30:57 -0300

    [web] feat: add deleted albums section to home page

--- web/src/app/(pages)/(private)/(home)/page.tsx
+++ web/src/app/(pages)/(private)/(home)/page.tsx
@@ -10,15 +10,21 @@ import { AlbumCardSkeleton } from './album-card-skeleton'
 import { NewAlbumModal } from './new-album-modal'
 
 export default function Home() {
-  const { data: albums, isLoading } = useQuery({
+  const { data: albums, isLoading: isAlbumsLoading } = useQuery({
     queryFn: async () => await getAlbums(),
     queryKey: ['albums'],
     staleTime: 1000 * 60 * 60 * 1, // 1 hours
   })
 
+  const { data: deletedAlbums, isLoading: isDeletedAlbumsLoading } = useQuery({
+    queryFn: async () => await getAlbums(true),
+    queryKey: ['deleted-albums'],
+    staleTime: 1000 * 60 * 60 * 1, // 1 hours
+  })
+
   return (
     <main className="m-auto min-h-screen w-full max-w-[1664px] px-8 py-6">
-      <section className="h-full">
+      <section>
         <div className="flex items-center justify-between">
           <h2 className="text-2xl font-bold">Albums</h2>
 
@@ -33,14 +39,14 @@ export default function Home() {
 
         <div
           className={clsx(
-            'mt-4 grid h-full grid-cols-1 justify-between gap-6 md:grid-cols-5',
+            'mt-4 grid grid-cols-1 gap-6 md:grid-cols-5',
             albums &&
             albums.length === 0 &&
-            !isLoading &&
+            !isAlbumsLoading &&
             'mt-[150px] md:grid-cols-1',
           )}
         >
-          {isLoading ? (
+          {isAlbumsLoading ? (
             Array.from({ length: 5 }).map((_: unknown, index: number) => (
               <AlbumCardSkeleton key={index} />
             ))
@@ -50,6 +56,38 @@ export default function Home() {
             ))
           ) : (
             <p className="text-center text-gray-500">No albums found</p>
+          )}
+        </div>
+      </section>
+
+      <section className="h-full mt-40">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-bold">Deleted Albums</h2>
+        </div>
+
+        <div
+          className={clsx(
+            'mt-4 grid grid-cols-1 gap-6 md:grid-cols-5',
+            deletedAlbums &&
+            deletedAlbums.length === 0 &&
+            !isDeletedAlbumsLoading &&
+            'mt-[150px] md:grid-cols-1',
+          )}
+        >
+          {isDeletedAlbumsLoading ? (
+            Array.from({ length: 5 }).map((_: unknown, index: number) => (
+              <AlbumCardSkeleton key={index} />
+            ))
+          ) : deletedAlbums && deletedAlbums.length > 0 ? (
+            deletedAlbums.map((album: any) => (
+              <AlbumCard
+                key={album.id}
+                album={album}
+                isDeletedAlbum={true}
+              />
+            ))
+          ) : (
+            <p className="text-center text-gray-500">No deleted albums found</p>
           )}
         </div>
       </section>
