452fe31402f495d1c8bffc2261cee25c2ed8d828
Eduardo Gomes <edugomesf@icloud.com>
2024-06-08 11:41:45 -0300

    [web] feat: create AlbumsGrid component and use it to display albums in home page

---
+++ web/src/app/(pages)/(private)/(home)/albums-grid.tsx
@@ -0,0 +1,43 @@
+import clsx from "clsx"
+
+import { Album } from "@/app/api/get-albums"
+import { AlbumCard } from "./album-card"
+import { AlbumCardSkeleton } from "./album-card-skeleton"
+
+interface AlbumsGridProps {
+  albums: Album[]
+  isLoading: boolean
+}
+
+export function AlbumsGrid({ isLoading, albums }: AlbumsGridProps) {
+
+  if (albums && albums.length === 0 && !isLoading) {
+    return (
+      <div className="flex items-center justify-center my-40">
+        <p className="text-center text-gray-600 text-lg">No albums found</p>
+      </div>
+    )
+  }
+
+  return (
+    <div
+      className={clsx(
+        'mt-4 h-auto grid grid-cols-1 gap-6 md:grid-cols-5 auto-rows-auto',
+        albums &&
+        albums.length === 0 &&
+        !isLoading &&
+        'mt-[150px] md:grid-cols-1',
+      )}
+    >
+      {isLoading ? (
+        Array.from({ length: 5 }).map((_: unknown, index: number) => (
+          <AlbumCardSkeleton key={index} />
+        ))
+      ) : (
+        albums.map((album: any) => (
+          <AlbumCard key={album.id} album={album} />
+        ))
+      )}
+    </div>
+  )
+}
--- web/src/app/(pages)/(private)/(home)/page.tsx
+++ web/src/app/(pages)/(private)/(home)/page.tsx
@@ -1,23 +1,21 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
-import clsx from 'clsx'
 
 import { getAlbums } from '@/app/api/get-albums'
 
-import { AlbumCard } from './album-card'
-import { AlbumCardSkeleton } from './album-card-skeleton'
 import { NewAlbumModal } from './new-album-modal'
 import { Button } from '@/app/components/button'
+import { AlbumsGrid } from './albums-grid'
 
 export default function Home() {
-  const { data: albums, isLoading: isAlbumsLoading } = useQuery({
+  const { data: albums = [], isLoading: isAlbumsLoading } = useQuery({
     queryFn: async () => await getAlbums(),
     queryKey: ['albums'],
     staleTime: 1000 * 60 * 60 * 1, // 1 hours
   })
 
-  const { data: deletedAlbums, isLoading: isDeletedAlbumsLoading } = useQuery({
+  const { data: deletedAlbums = [], isLoading: isDeletedAlbumsLoading } = useQuery({
     queryFn: async () => await getAlbums(true),
     queryKey: ['deleted-albums'],
     staleTime: 1000 * 60 * 60 * 1, // 1 hours
@@ -38,27 +36,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div
-          className={clsx(
-            'mt-4 h-auto grid grid-cols-1 gap-6 md:grid-cols-5 auto-rows-auto',
-            albums &&
-            albums.length === 0 &&
-            !isAlbumsLoading &&
-            'mt-[150px] md:grid-cols-1',
-          )}
-        >
-          {isAlbumsLoading ? (
-            Array.from({ length: 5 }).map((_: unknown, index: number) => (
-              <AlbumCardSkeleton key={index} />
-            ))
-          ) : albums && albums.length > 0 ? (
-            albums.map((album: any) => (
-              <AlbumCard key={album.id} album={album} />
-            ))
-          ) : (
-            <p className="text-center text-gray-500">No albums found</p>
-          )}
-        </div>
+        <AlbumsGrid
+          albums={albums}
+          isLoading={isAlbumsLoading}
+        />
       </section>
 
       <section className="h-full mt-20">
@@ -66,31 +47,10 @@ export default function Home() {
           <h2 className="text-2xl font-bold">Deleted Albums</h2>
         </div>
 
-        <div
-          className={clsx(
-            'mt-4 grid grid-cols-1 gap-6 md:grid-cols-5',
-            deletedAlbums &&
-            deletedAlbums.length === 0 &&
-            !isDeletedAlbumsLoading &&
-            'mt-[150px] md:grid-cols-1',
-          )}
-        >
-          {isDeletedAlbumsLoading ? (
-            Array.from({ length: 5 }).map((_: unknown, index: number) => (
-              <AlbumCardSkeleton key={index} />
-            ))
-          ) : deletedAlbums && deletedAlbums.length > 0 ? (
-            deletedAlbums.map((album: any) => (
-              <AlbumCard
-                key={album.id}
-                album={album}
-                isDeletedAlbum={true}
-              />
-            ))
-          ) : (
-            <p className="text-center text-gray-500">No deleted albums found</p>
-          )}
-        </div>
+        <AlbumsGrid
+          albums={deletedAlbums}
+          isLoading={isDeletedAlbumsLoading}
+        />
       </section>
     </main>
   )
