56969ee7bf9cbfc4f13bcbc769f2d7a6a04a4a4d
Eduardo Gomes <edugomesf@icloud.com>
2024-08-11 11:44:18 -0300

    [user] fix: update error code to unauthorized in GetAuthInfoFromHeaders middleware

--- user/src/presentation/rest/middleware/get-auth-info-from-headers.ts
+++ user/src/presentation/rest/middleware/get-auth-info-from-headers.ts
@@ -16,14 +16,14 @@ const tokenValidator = generateJwtTokenValidator()
 export async function getAuthInfoFromHeaders(req: Request, res: Response, next: NextFunction): Promise<any> {
   if (!req.headers.cookie) {
     logger.error('Cookies not found')
-    return res.status(403).send('Cookies not found')
+    return res.status(401).send('Cookies not found')
   }
 
   const { accessToken } = cookie.parse(req.headers.cookie)
 
   if (!accessToken) {
     logger.error('Invalid cookie format')
-    return res.status(403).send('Invalid cookie format')
+    return res.status(401).send('Invalid cookie format')
   }
 
   const { isValid, data, invalidationReason } = await tokenValidator.validate(accessToken)
@@ -31,14 +31,14 @@ export async function getAuthInfoFromHeaders(req: Request, res: Response, next: 
   if (!isValid) {
     logger.error('Auth token invalid or expired')
     const message = invalidationReason === 'EXPIRED' ? 'Token expired' : 'Token invalid'
-    return res.status(403).send(message)
+    return res.status(401).send(message)
   }
 
   const { id: userId } = data
 
   if (!userId) {
     logger.error('User id not found in token')
-    return res.status(403).send('User id not found in token')
+    return res.status(401).send('User id not found in token')
   }
 
   req.auth = { userId }
