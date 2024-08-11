08a1b508677f74b5102e09d20e92d533369d28d3
Eduardo Gomes <edugomesf@icloud.com>
2024-08-10 21:38:00 -0300

    [file-manager] test: add unit tests for DownloadFiles use case

--- file-manager/tests/unit/application/mock/file.mock.ts
+++ file-manager/tests/unit/application/mock/file.mock.ts
@@ -10,3 +10,26 @@ export const getFileMock = () => ({
   createdAt: new Date().toString(),
   updatedAt: new Date().toString()
 })
+
+export const getFileWithAlbumMock = () => ({
+  id: 'any-id',
+  name: 'any-file',
+  size: 1000,
+  type: 'image/jpeg',
+  extension: 'jpeg',
+  albumId: 'any-album-id',
+  url: 'www.any-url.com',
+  status: 'ACTIVE',
+  createdAt: new Date().toString(),
+  updatedAt: new Date().toString(),
+  album: {
+    id: 'any-id',
+    name: 'any-name',
+    isMain: true,
+    isDeleted: false,
+    status: 'ACTIVE',
+    createdAt: new Date().toString(),
+    updatedAt: new Date().toString(),
+    userId: 'user-id'
+  }
+})
