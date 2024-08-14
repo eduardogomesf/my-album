da76b54aef2fec70edf20e875c7d3a658cd60d1e
Eduardo Gomes <edugomesf@icloud.com>
2024-08-13 23:03:25 -0300

    [web] feat: add refresh token logic to axios response interceptor

--- web/src/app/lib/axios.ts
+++ web/src/app/lib/axios.ts
@@ -1,6 +1,42 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosRequestConfig } from 'axios'
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as AxiosRequestConfig & {
+      _retry?: boolean
+    }
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      error.response.data === 'Token expired' &&
+      !originalRequest?._retry
+    ) {
+      originalRequest._retry = true
+
+      try {
+        await api.post(
+          '/refresh-token',
+          {},
+          {
+            withCredentials: true,
+          },
+        )
+
+        return api(originalRequest)
+      } catch (refreshError) {
+        return Promise.reject(refreshError)
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export { api }
