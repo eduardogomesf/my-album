f6d55cb248d929ffcd34a3fdaeba346c16587750
Eduardo Gomes <edugomesf@icloud.com>
2024-10-06 09:00:00 -0300

    [web] feat: add upload result description to UploadOverlay component

--- web/src/app/(pages)/(private)/albums/[id]/upload-overlay.tsx
+++ web/src/app/(pages)/(private)/albums/[id]/upload-overlay.tsx
@@ -8,8 +8,10 @@ interface UploadOverlayProps {
   open: boolean
   onClose: () => void
   totalNumberOfFiles: number
-  finishedNumberOfFiles: number
+  numberOfUploadedFiles: number
+  numberOfFailedFiles: number
   filesToBeUploaded: FileWithId[]
+  isUploadInProgress: boolean
 }
 
 export function UploadOverlay({
@@ -17,31 +19,54 @@ export function UploadOverlay({
   open,
   onClose,
   totalNumberOfFiles,
-  finishedNumberOfFiles = 0,
+  numberOfUploadedFiles = 0,
+  isUploadInProgress = false,
+  numberOfFailedFiles = 0
 }: UploadOverlayProps) {
   return (
     <Dialog.Root open={open}>
       <Dialog.Portal>
-        <Dialog.Content className="fixed bottom-20 left-10 h-[300px] w-10/12 animate-fade-in-down rounded-md bg-white px-2 py-2 focus:outline-none md:w-[400px]">
+        <Dialog.Content
+          className="fixed bottom-20 left-10 h-[300px] w-10/12 animate-fade-in-down rounded-md bg-white px-2 py-2 focus:outline-none md:w-[400px]"
+        >
           <div className="flex items-center gap-1 px-2">
             <Dialog.Title className="text-lg font-semibold text-gray-800">
               Media Upload
             </Dialog.Title>
 
             <span className="text-sm font-normal text-gray-500">
-              ({finishedNumberOfFiles} of {totalNumberOfFiles})
+              ({numberOfUploadedFiles} of {totalNumberOfFiles})
             </span>
           </div>
 
-          <Dialog.Close asChild>
-            <button
-              className="group absolute right-4 top-2 flex h-6 w-6 items-center justify-center rounded-full transition duration-150 ease-in-out hover:bg-gray-200"
-              aria-label="Close"
-              onClick={onClose}
-            >
-              <X className="h-5 w-5 text-gray-700 transition duration-150 ease-in-out group-hover:text-gray-800" />
-            </button>
-          </Dialog.Close>
+          {!isUploadInProgress && (
+            <div className='flex flex-col items-start gap-1 px-2'>
+              {
+                numberOfUploadedFiles > 0 &&
+                (<span className='text-sm font-normal text-gray-500'>
+                  {numberOfUploadedFiles} files uploaded
+                </span>)
+              }
+              {
+                numberOfFailedFiles > 0 &&
+                (<span className='text-sm font-normal text-gray-500'>
+                  {numberOfFailedFiles} files failed to be uploaded
+                </span>)
+              }
+            </div>
+          )}
+
+          {!isUploadInProgress && (
+            <Dialog.Close asChild>
+              <button
+                className="group absolute right-4 top-2 flex h-6 w-6 items-center justify-center rounded-full transition duration-150 ease-in-out"
+                aria-label="Close"
+                onClick={onClose}
+              >
+                <X className="h-5 w-5 text-gray-700 transition duration-150 ease-in-out group-hover:text-gray-800" />
+              </button>
+            </Dialog.Close>
+          )}
 
           <div className="mb-4 mt-2 h-[2px] w-full bg-gray-100" />
 
